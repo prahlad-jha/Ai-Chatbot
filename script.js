f965d3ea9dc94f481a84c3277c9a9cab97bbe8f3
prahlad-jha <madhav38506@gmail.com>
2025-02-19 18:45:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let chatContainer = document.querySelector(".chat-container")
 let imagebtn = document.querySelector("#image");
 let image = document.querySelector("#image img");
 let imageinput = document.querySelector("#image input")
-const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=user Api key"
+const Api_Url = "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=Api key"
 
 let user={
     message:null,
